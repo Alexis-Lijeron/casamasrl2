93e44d75f29ad2dd265bd4e79d9d5e61a0c32643
Alexis-lijeron <alexislijeron.a@gmail.com>
2025-02-10 09:34:15 -0400

    a

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,4 +2,5 @@ export const environment = {
   URL_BACKEND: 'http://127.0.0.1:5000/',
   URL_SERVICIOS: 'http://localhost:5000/api',
   URL_FRONTEND: 'http://localhost:4200/',
+  URL_INVENTARIO:'http://localhost:5002/api'
 };
